0dc9b4568be1ed6294bdd957e9a895a4340151d9
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-11-04 00:07:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2015,7 +2015,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_sticker("CAACAgIAAxkBAAEMwYllwqNUvJlkLg9mEI7Z6ZAEG5AL6gAC5goAAhSjKEg_eSb4i9WKQh4E")
+            m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
             search = search.lower()
             find = search.split(" ")
             search = ""
