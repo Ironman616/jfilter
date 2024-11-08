78c5eb64f63072221fc9222edc7e3649762957fa
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-11-08 13:55:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,8 +105,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else (APP_NAME or '') + '.herokuapp.com'
-URL = "bots.hostingup.icu:8087/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "bots.hostingup.icu:8087/".format(FQDN, PORT)
+URL = "https://loki-8-nov-2024-bot-v2-7f49cf08705b.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://loki-8-nov-2024-bot-v2-7f49cf08705b.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "bots.hostingup.icu:8087/".format(FQDN)
+    URL = "https://loki-8-nov-2024-bot-v2-7f49cf08705b.herokuapp.com/".format(FQDN)
 else:
-    URL = "bots.hostingup.icu:8087/".format(FQDN)
+    URL = "https://loki-8-nov-2024-bot-v2-7f49cf08705b.herokuapp.com/".format(FQDN)
 
 
 
